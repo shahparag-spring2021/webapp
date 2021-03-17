2084ad4821b51b52b2bb89b96c6077b0ac096850
Parag Shah <paragshah367@gmail.com>
2021-03-16 22:31:50 -0700

    Test actions - modifying start_server, requirements.txt

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,6 @@ cd /home/ubuntu/webapp/
 
 # sudo kill -9 $(sudo lsof -t -i:4000);
 # sudo nohup npm start > /dev/null 2> /dev/null < /dev/null &
-sudo export FLASK_APP=app.py
+cd app/
+export FLASK_APP=app.py
 sudo flask run -h 0.0.0.0 -p 5000
