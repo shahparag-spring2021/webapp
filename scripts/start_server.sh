972744b88b6344c4861d05fb06043d1fbb57f938
Parag Shah <paragshah367@gmail.com>
2021-03-16 23:07:33 -0700

    Test actions - adding kill process, t7

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/webapp/
 
-# sudo kill -9 $(sudo lsof -t -i:4000);
-# sudo nohup npm start > /dev/null 2> /dev/null < /dev/null &
+sudo kill $(sudo lsof -t -i:5000)
 cd app/
 export FLASK_APP=app.py
-sudo flask run -h 0.0.0.0 -p 5000
+flask run -h 0.0.0.0 -p 5000
