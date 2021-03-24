c466de135ae1357012dc81ce7d4497422239d3ee
Parag Shah <paragshah367@gmail.com>
2021-03-24 15:23:31 -0700

    Adding cloudwatch config

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo 'start_server'
+cd /home/ubuntu/webapp/
+pwd
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/ubuntu/webapp/cloudwatch-config.json \
+    -s
+
 cd /home/ubuntu/webapp/app/
 pwd
+
 export FLASK_APP=app.py
 flask run -h 0.0.0.0 -p 5000 > /dev/null 2> /dev/null < /dev/null &
