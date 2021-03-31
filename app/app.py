48ee0f33a27ecdac569e4c525f20ed228ba6cd3b
Parag Shah <paragshah367@gmail.com>
2021-03-31 15:26:12 -0700

    A7 - Added /health and changes code deployment to OneAtATime

--- app/app.py
+++ app/app.py
@@ -41,7 +41,7 @@ migrate = Migrate(app, db)
 ma = Marshmallow(app)
 auth = HTTPBasicAuth()
 
-logging.basicConfig(filename='/home/ubuntu/webapp/app/csye6225.log', level=logging.DEBUG,
+logging.basicConfig(filename='/home/ubuntu/webapp/app/csye6225.log', level=logging.INFO,
                     format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 # SQLite Database
